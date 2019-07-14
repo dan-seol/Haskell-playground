7a2b0918c82294a3706da08b5e1e3a51c39b6f6f
Dan Seol <yunheum.seol@gmail.com>
2019-07-13 22:11:03 -0400

    logic error fixed

--- TRF.hs
+++ TRF.hs
@@ -2,15 +2,8 @@
 -- Strict evaluate to prevent haskell lazy loading thunks
 {-# LANGUAGE BangPatterns #-}
 
---Tail recursive factorial
-fact' :: Int -> Integer
-fact' n = fact'' n 1
-          where
-            fact'' :: Int -> Int -> Integer
-            fact'' 0 _ = 1
-            fact'' 1 _ = 1
-            fact'' a b = fact'' (a-1) (a*b)
-            
-            
+fact' :: Integer -> Integer
+fact' 0 = 1
+fact' n = product [1..n]
 main :: IO ()
-main = print 5
+main = print (fact' 10)
--- prac1_tcofac.hs
+++ prac1_tcofac.hs
@@ -1,13 +1,14 @@
+-- Strict evaluate to prevent haskell lazy loading thunks
 {-# LANGUAGE BangPatterns #-}
 -- Haskell version of TCO factorial
+
+facc :: Integer -> Integer -> Integer
+facc 0 _ = 1
+facc 1 b = b
+facc a b = facc (a-1) (a*b)
+
 fac :: Integer -> Integer
-fac n = factt n 1 
-    where
-        factt :: Integer -> Integer -> Integer
-        factt 0 _ = 1
-        factt 1 _ = 1
-        factt a b = factt (a-1) (a*b)
+fac n = facc n 1
 
-
-main :: IO ()
-main = print (fac 5)
+main :: IO()
+main = print (fac 10) 
